1a03a5204a8ec3f06adb48084e1c42dfd6e71feb
bgoldfedder <brandon@goldfedder.com>
2023-08-16 17:43:26 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -67,40 +67,40 @@ def main():
     process_button = st.sidebar.button("Process Podcast Feed")
     st.sidebar.markdown("**Note**: Podcast processing can take up to 5 mins, please be patient.")
 
-      if process_button:
-
+    if process_button:
+    
         # Call the function to process the URLs and retrieve podcast guest information
         podcast_info = process_podcast_info(url)
-
+    
         # Right section - Newsletter content
         st.header("Newsletter Content")
-
+    
         # Display the podcast title
         st.subheader("Episode Title")
         st.write(podcast_info['podcast_details']['episode_title'])
-
+    
         # Display the podcast summary and the cover image in a side-by-side layout
         col1, col2 = st.columns([7, 3])
-
+    
         with col1:
             # Display the podcast episode summary
             st.subheader("Podcast Episode Summary")
             st.write(podcast_info['podcast_summary'])
-
+    
         with col2:
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
+    
         # Display the podcast guest and their details in a side-by-side layout
         col3, col4 = st.columns([3, 7])
-
+    
         with col3:
             st.subheader("Podcast Guest")
             st.write(podcast_info['podcast_guest']['name'])
-
+    
         with col4:
             st.subheader("Podcast Guest Details")
             st.write(podcast_info["podcast_guest"]['summary'])
-
+    
         # Display the five key moments
         st.subheader("Key Moments")
         key_moments = podcast_info['podcast_highlights']
